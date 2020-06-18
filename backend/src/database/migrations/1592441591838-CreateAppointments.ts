3e9b85ebba218de5f09c2afe457896cf4796f18a
Eduardo Godoy <dudurossigodoy2010@gmail.com>
2020-06-18 01:05:35 -0300

    Adjust function with database

--- backend/src/database/migrations/1592441591838-CreateAppointments.ts
+++ backend/src/database/migrations/1592441591838-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1592441591838
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
