86872bbc0eb51183370199d75ed3b6d58af2e7bd
Eduardo Godoy <dudurossigodoy2010@gmail.com>
2020-06-23 01:28:18 -0300

    Adjust Repository

---
+++ backend/src/modules/users/infra/http/routes/sessions.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+import UsersController from '../controllers/SessionsController';
+
+const sessionsRouter = Router();
+const usersController = new UsersController();
+
+sessionsRouter.post('/', usersController.create);
+
+export default sessionsRouter;
