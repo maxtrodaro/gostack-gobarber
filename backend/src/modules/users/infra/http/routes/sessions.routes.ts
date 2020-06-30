ca26ef4336dde892828923ec855f5691aa48eaf7
Eduardo Godoy <dudurossigodoy2010@gmail.com>
2020-06-30 20:13:03 -0300

    Validations requests

--- backend/src/modules/users/infra/http/routes/sessions.routes.ts
+++ backend/src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
-import UsersController from '../controllers/SessionsController';
+import SessionsController from '../controllers/SessionsController';
 
 const sessionsRouter = Router();
-const usersController = new UsersController();
+const sessionsController = new SessionsController();
 
-sessionsRouter.post('/', usersController.create);
+sessionsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  sessionsController.create,
+);
 
 export default sessionsRouter;
