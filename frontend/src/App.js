8ef1127853f807287dd526d89475ac5088cb6a83
Eduardo Godoy <dudurossigodoy2010@gmail.com>
2020-07-02 21:05:42 -0300

    Simple API Request React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+import api from "./services/api";
+import "./App.css";
 import Header from "./components/Header";
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get("/projects").then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
+
+  // projects = Valor atual do array
+  // setProjects = Retorna o estado e atualiza as informações
+
+  function handleAddProject() {
+    setProjects([...projects, `New project ${Date.now()}`]);
+  }
+
   return (
     <>
-      <Header title="Homepage">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
-      <Header title="Projects">
-        <ul>
-          <li>Teste</li>
-          <li>Testando</li>
-        </ul>
-      </Header>
+      <Header title="Projects" />
+
+      <ul>
+        {projects.map((project) => (
+          <li key={project.id}>{project.title}</li>
+        ))}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>
+        Adicionar projeto
+      </button>
     </>
   );
 }
